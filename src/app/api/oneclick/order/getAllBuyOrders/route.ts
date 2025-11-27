53e4b662f9f2a7a10f0eb103e03446055ff6f8d2
seunghyonpark <creath.park@gmail.com>
2025-11-27 13:35:41 +0900

    feat: update API endpoint for fetching buy orders to use consistent URL structure

--- src/app/api/oneclick/order/getAllBuyOrders/route.ts
+++ src/app/api/oneclick/order/getAllBuyOrders/route.ts
@@ -41,14 +41,14 @@ export async function POST(request: NextRequest) {
 
 
   // call api
-  let apiUrl = `${stableUrlStable}/api/order/getAllBuyOrdersBySellerAccountNumber`;
+  let apiUrl = `${stableUrlStable}/api/order/getAllBuyOrders`;
 
   if (stabilityId === '150b53f165222304af7c45dc45c73863') {
-    apiUrl = `${stableUrlStable}/api/order/getAllBuyOrdersBySellerAccountNumber`;
+    apiUrl = `${stableUrlStable}/api/order/getAllBuyOrders`;
   } else if (stabilityId === '9ed089930921bfaa1bf65aff9a75fc41') {
-    apiUrl = `${stableUrlGeorgia}/api/order/getAllBuyOrdersBySellerAccountNumber`;
+    apiUrl = `${stableUrlGeorgia}/api/order/getAllBuyOrders`;
   } else if (stabilityId === 'd140e95d67da87ff62efabf401171aa0') {
-    apiUrl = `${stableUrlXlay}/api/order/getAllBuyOrdersBySellerAccountNumber`;
+    apiUrl = `${stableUrlXlay}/api/order/getAllBuyOrders`;
   }
 
 
